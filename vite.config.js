f62b380337a3277bfaa120c375057fa5384be070
rotxiv <117417281+rotxiv@users.noreply.github.com>
2025-10-29 00:49:02 -0400

    Set base URL based on environment variable

--- vite.config.js
+++ vite.config.js
@@ -16,4 +16,5 @@ export default defineConfig({
         cors: true,
         https: true, // Fuerza HTTPS en el servidor de desarrollo
     },
+    base: process.env.NODE_ENV === 'production' ? process.env.APP_URL + '/' : '/',
 });
